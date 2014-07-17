54f32fa05a0f7af4b7d5213cbb8e423eb3e85add
Paul Austin <paul.austin@revolsys.com>
2014-07-17 13:03:57 -0700

    2014.07.17-CPF1

--- com.revolsys.open.swing/src/main/java/com/revolsys/swing/map/overlay/AddGeometryCompleteAction.java
+++
@@ -1,7 +0,0 @@
-package com.revolsys.swing.map.overlay;
-
-import com.vividsolutions.jts.geom.Geometry;
-
-public interface AddGeometryCompleteAction {
-  void addComplete(AbstractOverlay overlay, Geometry geometry);
-}
